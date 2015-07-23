def246fffc1f2c9692182fcfdeb4f045cc96edbd
iamnancy07 <iamnancy07@users.noreply.github.com>
2015-07-23 11:07:20 -0700

    resubmit cache inverse matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,32 +4,33 @@
 ## get value of inverse matrix
 
 
-makeCacheMatrix <- function(x = matrix()) {
-        i <- NULL
-        set <- function(y) {
-            x << y
-            i << NULL
-        }
-        getvalue <- function() x
-        setinverse <- function(inverse) i <<- inverse
-        getinverse <- function() i
-        list(set = set, getvalue = getvalue, setinverse = setinverse, getinverse = getinverse
+makeCacheMatrix <- function(x = matrix())  {
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    getvalue <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, getvalue = getvalue, setinverse = setinverse, getinverse = getinverse)
+    
 }
 
 
 ## calculate inverse if inverse of matrix is never calculated before; otherwise, get it from cacheSolve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        i <- x$getinverse()
-        if(!is.null(i)){
-                message("getting cached data")
-                return(i)
-        }
-        data <- x$getvalue()
-        i <- solve(data)
-        x$setinverse(i)
-        i
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)){
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$getvalue()
+    i <- solve(data)
+    x$setinverse(i)
+    i
 }
 
 
